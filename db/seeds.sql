958a5e5b4478a63e05763d5cec680c804588e6f5
schmartmann <stefanhartmann@gmail.com>
2019-02-08 10:59:45 -0500

    videos now belong to a brand

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO brands ( name, created ) VALUES ( 'StefanVision', '2019-02-01T15:04:06.074Z' );
+
+INSERT INTO videos ( name, brand_id, published ) VALUES ( 'Unlikely Best Friend: Old Man & Osteoblastoma', 1, '2019-02-07T15:04:06.074Z');
